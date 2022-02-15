c431680d9ca2577564e19b0004e77a530699fbc0
CheonSeungEon <tmddjs9565@gmail.com>
2022-02-15 15:47:43 +0900

    #3.6 Themes

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme:DefaultTheme={
+    bgColor:"white",
+    textColor:"black",
+    btnColor : "tomaot",
+};
+
+export const darkTheme:DefaultTheme={
+    bgColor:"black",
+    textColor:"white",
+    btnColor : "teal",
+};
